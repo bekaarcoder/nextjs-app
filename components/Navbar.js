597d7cfa41a3d4063792943c7e6a3238270a3334
Shashank <s_shashank@outlook.com>
2020-12-19 23:57:17 +0530

    Next js app setup

---
+++ components/Navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          NextRead
+        </a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+          </ul>
+          <ul className="d-flex navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#">
+                Sign In
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Register
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
